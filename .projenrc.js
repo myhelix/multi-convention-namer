c93a0ff1527859a6dbce6f15a77d668a8a7e4a2c
Andrew Hammond <andrew.hammond@helix.com>
2021-06-11 15:49:13 -0700

    projen new

---
+++ .projenrc.js
@@ -0,0 +1,13 @@
+const { TypeScriptProject } = require('projen');
+const project = new TypeScriptProject({
+  defaultReleaseBranch: 'main',
+  name: 'namer',
+
+  // deps: [],                          /* Runtime dependencies of this module. */
+  // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],                       /* Build dependencies for this module. */
+  // packageName: undefined,            /* The "name" in package.json. */
+  // projectType: ProjectType.UNKNOWN,  /* Which type of project this is (library/app). */
+  // releaseWorkflow: undefined,        /* Define a GitHub workflow for releasing from "main" when new versions are bumped. */
+});
+project.synth();
